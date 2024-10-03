a466ec425a4326d510ad4a9bda403b60bb87f188
GSetteF <gabrielsettef@gmail.com>
2024-10-03 15:03:47 -0300

    feat: created Characters routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from './pages/Login';
+import { Characters } from "./pages/Characters";
 
 function App() {
 
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
-      
+      <Route path="/Characters" element={<Characters />} />
       </Routes>
   </BrowserRouter>;
 }
