8d1a68a307e6c47601dc65c995a8e36c58d38a69
elionGal <eliongalvao@gmail.com>
2024-10-02 21:28:25 -0300

    feat: ComicList.tsx's structure HTML base complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from './pages/Login';
+import ComicsList from "./Components/ComicList";
 
 function App() {
 
   return <BrowserRouter>
+  <ComicsList />
     <Routes>
       <Route path="/" element={<Login />} />
-      
       </Routes>
   </BrowserRouter>;
 }
