601b548f06902553509c6b6b4d8dab95ebccdd21
jeff <junior.fidelis.3386@gmail.com>
2024-10-04 11:37:30 -0300

    ✨ feat(header): finalize the implementation of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from './pages/Login';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header/Header'; 
+import Login from './pages/Login'; 
 
-function App() {
-
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      
-      </Routes>
-  </BrowserRouter>;
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                {/* Renderizar o Header apenas se a rota não for '/login' ou '/register' ou '/' */}
+                {window.location.pathname !== '/login' && window.location.pathname !== '/register' && window.location.pathname !== '/' && <Header />}
+                
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/Characters" element={<Characters />} />
+                   
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
