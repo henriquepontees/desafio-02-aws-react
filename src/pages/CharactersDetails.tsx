7363dbf98fd1b1949d1deb7b125357b0d1bd489a
GSetteF <gabrielsettef@gmail.com>
2024-10-04 15:29:43 -0300

    feat: fully implemented API calls

--- src/pages/CharactersDetails.tsx
+++ src/pages/CharactersDetails.tsx
@@ -25,7 +25,53 @@ export const CharactersDetails = () => {
   const [character, setCharacter] = useState<Character | null>(null);
   const apiKey = '6fc18222ac8e9167cd6f3fb05cff423b';
 
- 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+        const generalResponse = await axios.get(
+          `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${apiKey}`
+        );
+
+        const storiesResponse = await axios.get(
+            `https://gateway.marvel.com:443/v1/public/characters/${id}/comics?limit=20&apikey=${apiKey}`
+          );
+
+        const comicsResponse = await axios.get(
+            `https://gateway.marvel.com:443/v1/public/comics?limit=20&apikey=${apiKey}`
+        );
+
+        const charInfo = generalResponse.data.data.results[0];
+        const storiesInfo = storiesResponse.data.data;
+        const comicsInfo = comicsResponse.data.data;
+
+        const characterData: Character ={
+          name: charInfo.name,
+          image: `${charInfo.thumbnail.path}/portrait_xlarge.jpg`,
+          created: charInfo.modified ? charInfo.modified.slice(0, 4) : "Data não disponível",
+          seriesNumber: charInfo.series.available === "" ? "Registro não existe." : charInfo.series.available,
+          storiesNumber: charInfo.stories.available === "" ? "Registro não existe." : charInfo.stories.available,
+          description: charInfo.description === "" ? "Registro não existe." : charInfo.description,
+          stories: storiesInfo.results.slice(0,3).map((comic: any) => ({
+            id: comic.id, 
+            name: comic.title,
+            image: comic.thumbnail.path+"/portrait_xlarge.jpg", 
+          })),
+          otherComics: comicsInfo.results.map((comic: any) => ({
+            id: comic.id, 
+            name: comic.title,
+            image: comic.thumbnail.path+"/portrait_xlarge.jpg", 
+          })),
+        };
+
+        setCharacter(characterData);
+      } catch (err: any) {
+        console.log(err);
+      }
+    };
+
+    fetchData(); 
+  }, []); 
   return (
     <div>
         <h1>Personagem: {character?.name || 'Carregando...'}</h1>
