76ae32d8b18c55cd954488fa75d10fee4a8eb7bb
Henrique Pontes <henriquepontes0405@gmail.com>
2024-10-05 00:30:10 -0300

    feat: add register in EnumRoutes

--- src/Utils/Functions.ts
+++ src/Utils/Functions.ts
@@ -1,5 +1,5 @@
 import { EnumRoutes } from "./Enums"
 
 export const HeaderEnableValidator = (path : string) => {
-   return path != EnumRoutes.HOME && path != EnumRoutes.LOGIN
+   return path != EnumRoutes.HOME && path != EnumRoutes.LOGIN && path != EnumRoutes.REGISTER
 }
