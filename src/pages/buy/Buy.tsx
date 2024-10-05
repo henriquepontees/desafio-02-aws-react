f9ddb14b2a79e9f0d20b29c7a3ca88fdb4c2c5f7
Gabriel Martins <135551189+GabrielSousaM@users.noreply.github.com>
2024-10-05 17:49:07 -0300

    :sparkles: feat: buy screen features added

--- src/pages/buy/Buy.tsx
+++ src/pages/buy/Buy.tsx
@@ -1,19 +1,59 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
 import '../../styles/buy.css'
-import AddressForm from './components/AddressForm'
 import PaymentMethod from './components/PaymentMethod'
+import { SlLocationPin } from "react-icons/sl"
+import axios from 'axios'
+import { AddressData } from '../../types/addressData'
+import { useAddressStore } from '../../store/useAddress'
 
 const Buy = () => {
+  const {register, handleSubmit, setValue, setFocus} = useForm<AddressData>()
+  const {addAddress} = useAddressStore()
+  const onBuy: SubmitHandler<AddressData> = data => {
+    addAddress(data)
+  }
+  const checkCep = async (e: React.FocusEvent<HTMLInputElement>) => {
+    const cep = e.target.value.replace(/\D/g, '')
+    if (!e.target.value) return
+    await axios.get(`https://viacep.com.br/ws/${cep}/json/`).then(res => {
+      setValue("street", res.data.logradouro)
+      setValue("district", res.data.bairro)
+      setValue("city", res.data.localidade)
+      setValue("state", res.data.uf)
+      setFocus("number")
+    }).catch(err => console.log(err))
+  }
+  
 
   return <>
     <header>
-      {/* Colocar Header no lugar dessa header */}
+      {/* PlaceHolder */}
     </header>
     <main>
-      <h1>Finalize a compra</h1>
-      <AddressForm />
-      <PaymentMethod />
-      <hr />
-      <button>Comprar</button>
+      <section id='buyTitle'>
+        <h1 id='title'>Finalize a compra</h1>
+      </section>
+      <section id='buyInfo'>
+        <section id="addressForm">
+          <h2 id="addressTitle"><SlLocationPin style={{color: "#FF8100"}}/>Endereço de Entrega</h2>
+          <p id="addressDescription">Informe o endereço onde deseja receber seu pedido</p>
+          <form>
+            <fieldset>
+              <input type="text" {...register("cep", {required: true})} onBlur={checkCep} id="cep" placeholder="CEP" className="formInput"/>
+              <input type="text" {...register("street", {required: true})} id="street" placeholder="Rua" className="formInput"/>
+              <input type="text" {...register("number", {required: true})} id="number" placeholder="Número" className="formInput"/>
+              <input type="text" {...register("complement", {required: false})} id="complement" placeholder="Complemento" className="formInput"/>
+              <input type="text" {...register("district", {required: true})} id="district" placeholder="Bairro" className="formInput"/>
+              <input type="text" {...register("city", {required: true})} id="city" placeholder="Cidade" className="formInput"/>
+              <input type="text" {...register("state", {required: true})} id="state" placeholder="Estado" className="formInput"/>
+            </fieldset>
+            <PaymentMethod />
+          </form>
+        </section>
+      </section>
+      <section id='buyButton'>
+        <button onClick={handleSubmit(onBuy)}>Comprar</button>
+      </section>
     </main>
   </>
 }
--- src/pages/buy/components/PaymentMethod.tsx
+++ src/pages/buy/components/PaymentMethod.tsx
@@ -1,16 +1,19 @@
 import { BsCurrencyDollar } from "react-icons/bs"
 import { CiBank } from "react-icons/ci"
 import { PiCreditCard, PiMoneyLight } from "react-icons/pi"
+import { usePaymentMethod } from "../../../store/usePayMethod"
 
 const PaymentMethod = () => {
+  const {setPaymentMethod} = usePaymentMethod()
+
   return <>
-    <section>
-        <h2><BsCurrencyDollar />Pagamento</h2>
-        <p>O pagamento é feito na entrega. Escolha a forma que desejar pagar.</p>
-        <button><PiCreditCard />CARTÃO DE CRÉDITO</button>
-        <button><CiBank/>CARTÃO DE DÉBITO</button>
-        <button><PiMoneyLight/>DINHEIRO</button>
-    </section>
+    <fieldset id="paymentSelection">
+      <legend id="paymentTitle"><BsCurrencyDollar style={{color: "#FF8100"}}/>Pagamento</legend>
+      <p id="paymentDescription">O pagamento é feito na entrega. Escolha a forma que desejar pagar.</p>
+      <button className="paymentMethod" onClick={() => setPaymentMethod("credit")}><PiCreditCard style={{color: "#FF8100"}}/>CARTÃO DE CRÉDITO</button>
+      <button className="paymentMethod" onClick={() => setPaymentMethod("debit")}><CiBank style={{color: "#FF8100"}}/>CARTÃO DE DÉBITO</button>
+      <button className="paymentMethod" onClick={() => setPaymentMethod("money")}><PiMoneyLight style={{color: "#FF8100"}}/>DINHEIRO</button>
+    </fieldset>
   </>
 }
 
