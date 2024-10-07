90f6cae540cabe17302afcf1365be4567033ed28
Henrique Pontes <henriquepontes0405@gmail.com>
2024-10-07 15:38:47 -0300

    feat/bugfix: add cart funcionality and fix bug in merge

--- src/Utils/Enums.ts
+++ src/Utils/Enums.ts
@@ -2,6 +2,7 @@ export enum EnumRoutes {
     HOME = "/",
     LOGIN = "/login",
     ComicsList = "/ComicsList",
+    ComicListDetails = "/ComicListDetails",
     CHARACTERS = "/Characters",
     CHARACTERSDETAILS = "/CharactersDetails",
     REGISTER = "/register",
