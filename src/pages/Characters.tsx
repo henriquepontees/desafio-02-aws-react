7af46a3ee57baceef0bd7534d6c2045d2d4fa4f1
Henrique Pontes <henriquepontes0405@gmail.com>
2024-10-08 15:22:57 -0300

    style: characters.tsx change

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -2,70 +2,70 @@ import { useState, useEffect, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import '/src/styles/Character.css';
-import { PUBLIC_KEY } from '../components/Commons';
-import Spinner from '../components/Spinner';
-
+import { PUBLIC_KEY } from '../Components/Commons';
+import Spinner from '../Components/Spinner';
+ 
 interface Character {
   id: number;
   image: string;
   name: string;
 }
-
+ 
 export const Characters = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
   const firstLoad = useRef(true);
   const navigate = useNavigate();
   const location = useLocation();
   const storage = localStorage;
-
+ 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true); 
-
+      setLoading(true);
+ 
       try {
         const params = new URLSearchParams(location.search);
         const searchQuery = params.get('search');
-
+ 
         if (firstLoad.current) {
           firstLoad.current = false;
-          return; 
+          return;
         }
-
+ 
         const response = await axios.get(
           `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${offset}&apikey=${PUBLIC_KEY}${searchQuery ? `&nameStartsWith=${searchQuery}` : ''}`
         );
-
+ 
         const characterData: Character[] = response.data.data.results.map((character: any) => ({
           id: character.id,
           name: character.name,
           image: `${character.thumbnail.path}/portrait_xlarge.jpg`,
         }));
-
+ 
         setCharacters((prevCharacters) => [...prevCharacters, ...characterData]);
       } catch (err: any) {
         console.log(err);
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
-
+ 
     fetchData();
   }, [offset, location.search]);
-
+ 
   const handleButtonClick = () => {
     setOffset((prevOffset) => prevOffset + 20);
   };
-
+ 
   const handleImageClick = (id: number) => {
     storage.setItem(`idImagem`, id.toString());
     navigate(`/characters/${id}`);
   };
-
+ 
   return (
     <div className='charactersGrid'>
-      {loading && <Spinner />} 
+      {loading && <Spinner />}
       <ul className="character-list">
         {characters.map((character: Character) => (
           <li key={character.id} className="character-card">
@@ -83,7 +83,7 @@ export const Characters = () => {
           </li>
         ))}
       </ul>
-      {!loading && characters.length > 0 && ( 
+      {!loading && characters.length > 0 && (
         <button onClick={handleButtonClick} className='button-characters'>+ Carregar mais</button>
       )}
     </div>
